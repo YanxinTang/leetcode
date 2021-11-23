2a709f7cbfe7ba1a1bf99d216f773623838c2524
tyx1703 <tyx1703@gmail.com>
2021-11-23 14:19:15 +0800

    746. 使用最小花费爬楼梯

---
+++ src/746.min-cost-climbing-stairs/index.test.js
@@ -0,0 +1,22 @@
+import minCostClimbingStairs from '.';
+
+test('746. 使用最小花费爬楼梯', () => {
+  const cases = [
+    {
+      input: [
+        [10, 15, 20]
+      ],
+      want: 15
+    },
+    {
+      input: [
+        [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+      ],
+      want: 6
+    },
+  ];
+
+  for (const tc of cases) {
+    expect(minCostClimbingStairs.apply(this, tc.input)).toBe(tc.want);
+  }
+});
