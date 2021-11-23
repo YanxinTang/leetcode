8537c80490bbe26a3a2b4f6f8cde825fd8d5bf49
tyx1703 <tyx1703@gmail.com>
2021-11-23 15:48:57 +0800

    增加创建问题的脚本

---
+++ scripts/new.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const argv = require('minimist')(process.argv.slice(2));
+const fs = require('fs/promises');
+
+const SRC_DIR = path.resolve(__dirname, '../src');
+
+let name;
+if (argv._.length <= 0) {
+  console.log('请输入题目');
+  process.exit(1);
+} else if (argv._.length > 1) {
+  console.log('参数过多');
+  process.exit(1);
+} else {
+  name = String(argv._[0]);
+}
+
+const testFileTemplate = `import func from '.';
+
+test('', () => {
+  const cases = [
+    {
+      input: [],
+      want: ''
+    }
+  ];
+  for (const tc of cases) {
+    expect(func.apply(this, tc.input)).toBe(tc.want);
+  }
+});
+`;
+
+async function newQuestion(name) {
+  const questionDir = path.join(SRC_DIR, name);
+  await fs.mkdir(questionDir)
+  await fs.writeFile(path.join(questionDir, 'index.js'), '');
+  await fs.writeFile(
+    path.join(questionDir, 'index.test.js'),
+    testFileTemplate,
+  );
+  return '创建成功';
+}
+
+newQuestion(name).then(console.log, console.error);
