cdfaa3cd174e49c81272646c941d653570a026a6
tyx1703 <tyx1703@gmail.com>
2021-11-27 17:15:17 +0800

    45. 跳跃游戏 II

---
+++ src/45.jump-game-ii/index.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const jump = function(nums) {
+  let end = 0; // 当前跳的最大位置
+  let max = 0; // 下一跳的最大位置
+  let n = 0;   // 已经跳的次数
+  for (let i = 0; i < nums.length - 1; i++) {
+    max = Math.max(max, i + nums[i]);
+    if (i === end) {
+      n++;
+      end = max;
+    }
+  }
+  return n;
+};
+
+export default jump;
---
+++ src/45.jump-game-ii/index.test.js
@@ -0,0 +1,51 @@
+import func from '.';
+
+test('', () => {
+  const cases = [
+    {
+      input: [
+        [2,3,1,1,4]
+      ],
+      want: 2
+    },
+    {
+      input: [
+        [2,3,0,1,4]
+      ],
+      want: 2
+    },
+    {
+      input: [
+        [0],
+      ],
+      want: 0
+    },
+    {
+      input: [
+        [2,1]
+      ],
+      want: 1
+    },
+    {
+      input: [
+        [1,2]
+      ],
+      want: 1
+    },
+    {
+      input: [
+        [1,2,1,1,1]
+      ],
+      want: 3
+    },
+    {
+      input: [
+        [2,3,1]
+      ],
+      want: 1
+    }
+  ];
+  for (const tc of cases) {
+    expect(func.apply(this, tc.input), JSON.stringify(tc.input)).toBe(tc.want);
+  }
+});
