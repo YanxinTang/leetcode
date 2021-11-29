e780cc9077c8f68d941b5dc74e912d0cd29e0e31
tyx1703 <tyx1703@gmail.com>
2021-11-29 19:00:48 +0800

    152. 乘积最大子数组

---
+++ problems/152.maximum-product-subarray/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxProduct = function(nums) {
+  let preMin = nums[0], preMax = preMin, max = preMin;
+  for (let i = 1; i < nums.length; i++) {
+    const num = nums[i];
+    const _preMin = preMin, _preMax = preMax;
+    preMin = Math.min(num, _preMin * num, _preMax * num);
+    preMax = Math.max(num, _preMin * num, _preMax * num);
+    max = Math.max(max, preMax);
+  }
+  return max;
+};
+
+export default maxProduct;
