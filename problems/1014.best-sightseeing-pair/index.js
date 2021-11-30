e16da3e569cfc3c38872db0e68e23472a6db6fc1
tyx1703 <tyx1703@gmail.com>
2021-11-30 15:21:26 +0800

    1014. 最佳观光组合

---
+++ problems/1014.best-sightseeing-pair/index.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} values
+ * @return {number}
+ * 
+ * values[i] + values[j] + i - j = (values[i] + i) + (values[j] - j)
+ * 
+ * 假设 f(i) 表示 i ∈ [0, i+1) 时 values[i] + i 的最大值
+ * 则有 f(i) = Max(f(i-1), values[i] + 1)
+ *
+ */
+const maxScoreSightseeingPair = function(values) {
+
+  let a = values[0]; // f(i)
+  let ans = values[0] + values[1] - 1;
+  for (let i = 2; i < values.length; i++) {
+    a = Math.max(values[i - 1] + i - 1, a);
+    ans = Math.max(ans, a + values[i] - i);
+  }
+  return ans;
+};
+
+export default maxScoreSightseeingPair;
---
+++ problems/1014.best-sightseeing-pair/index.test.js
@@ -0,0 +1,21 @@
+import func from '.';
+
+test('1014. 最佳观光组合', () => {
+  const cases = [
+    {
+      input: [
+        [8,1,5,2,6]
+      ],
+      want: 11
+    },
+    {
+      input: [
+        [1,2]
+      ],
+      want: 2
+    }
+  ];
+  for (const tc of cases) {
+    expect(func.apply(this, tc.input), JSON.stringify(tc.input)).toBe(tc.want);
+  }
+});
