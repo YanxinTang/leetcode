8a468ab6e79a4d1de1fde4497d34bba27f75182b
tyx1703 <tyx1703@gmail.com>
2021-11-30 09:42:29 +0800

    1567. 乘积为正数的最长子数组长度

---
+++ problems/1567.maximum-length-of-subarray-with-positive-product/index.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const getMaxLen = function(nums) {
+  // nums[0:i] 中，最后一个乘积为负数的子串长度
+  let preNegLen = nums[0] < 0 ? 1 : 0;
+  // nums[0:1] 中，最后一个乘积为正数的子串长度
+  let prePosLen = nums[0] > 0 ? 1 : 0;
+  
+  let max = prePosLen;
+  for (let i = 1; i < nums.length; i++) {
+    const num = nums[i];
+    if (num > 0) {
+      prePosLen++;
+      preNegLen = preNegLen > 0 ? preNegLen + 1 : 0;
+    } else if (num < 0) {
+      const t = prePosLen;
+      prePosLen = preNegLen > 0 ? preNegLen + 1 : 0;
+      preNegLen = t + 1;
+    } else {
+      preNegLen = prePosLen = 0;
+    }
+    max = Math.max(prePosLen, max);
+  }
+  return max;
+};
+
+export default getMaxLen;
---
+++ problems/1567.maximum-length-of-subarray-with-positive-product/index.test.js
@@ -0,0 +1,39 @@
+import func from '.';
+
+test('1567. 乘积为正数的最长子数组长度', () => {
+  const cases = [
+    {
+      input: [
+        [1,-2,-3,4]
+      ],
+      want: 4
+    },
+    {
+      input: [
+        [0,1,-2,-3,-4]
+      ],
+      want: 3
+    },
+    {
+      input: [
+        [-1,-2,-3,0,1]
+      ],
+      want: 2
+    },
+    {
+      input: [
+        [-1,2]
+      ],
+      want: 1
+    },
+    {
+      input: [
+        [1,2,3,5,-6,4,0,10]
+      ],
+      want: 4
+    }
+  ];
+  for (const tc of cases) {
+    expect(func.apply(this, tc.input), JSON.stringify(tc.input)).toBe(tc.want);
+  }
+});
