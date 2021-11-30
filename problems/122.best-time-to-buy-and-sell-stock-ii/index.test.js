324fa09f9334c2d97bf4ed86b7643be3408bc7c7
tyx1703 <tyx1703@gmail.com>
2021-11-30 16:16:49 +0800

    122. 买卖股票的最佳时机 II

---
+++ problems/122.best-time-to-buy-and-sell-stock-ii/index.test.js
@@ -0,0 +1,27 @@
+import func from '.';
+
+test('122. 买卖股票的最佳时机 II', () => {
+  const cases = [
+    {
+      input: [
+        [7,1,5,3,6,4]
+      ],
+      want: 7
+    },
+    {
+      input: [
+        [1,2,3,4,5]
+      ],
+      want: 4
+    },
+    {
+      input: [
+        [7,6,4,3,1]
+      ],
+      want: 0
+    }
+  ];
+  for (const tc of cases) {
+    expect(func.apply(this, tc.input), JSON.stringify(tc.input)).toBe(tc.want);
+  }
+});
