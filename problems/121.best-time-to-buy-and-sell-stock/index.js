36129030c2484fe72b3d0702a2e4a9868df58112
tyx1703 <tyx1703@gmail.com>
2021-11-30 15:49:01 +0800

    121. 买卖股票的最佳时机

---
+++ problems/121.best-time-to-buy-and-sell-stock/index.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+const maxProfit = function(prices) {
+  let pre = prices[0]; // 成本价
+  let max = 0;         // 最多能卖
+  for (let i = 1; i < prices.length; i++) {
+    max = Math.max(max, prices[i] - pre);
+    pre = pre < prices[i] ? pre : prices[i];
+  }
+  return max;
+};
+
+export default maxProfit;
