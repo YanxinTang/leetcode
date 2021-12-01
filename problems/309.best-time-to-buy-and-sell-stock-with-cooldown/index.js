3ac7cb1be12c2a4166de393562223c43081d9cd0
tyx1703 <tyx1703@gmail.com>
2021-12-01 10:50:33 +0800

    309. 最佳买卖股票时机含冷冻期

---
+++ problems/309.best-time-to-buy-and-sell-stock-with-cooldown/index.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+const maxProfit = function(prices) {
+  let a = 0; // 没股票，在冷冻
+  let b = 0; // 没股票，未冷冻
+  let c = -prices[0]; // 有股票
+  for (let i = 1; i < prices.length; i++) {
+    const price = prices[i];
+    const newA = c + price;             // 有股票，并在当天卖出
+    const newB = Math.max(b, a);        // 昨天有股票没在冷冻，咋天没股票在冷冻
+    const newC = Math.max(c, b - price);// 昨天有股票，昨天没股票并不在冷冻 - 购入今日股票
+    a = newA;
+    b = newB;
+    c = newC;
+  }
+  return Math.max(a, b);
+};
+
+export default maxProfit;
---
+++ problems/309.best-time-to-buy-and-sell-stock-with-cooldown/index.test.js
@@ -0,0 +1,15 @@
+import func from '.';
+
+test('309. 最佳买卖股票时机含冷冻期', () => {
+  const cases = [
+    {
+      input: [
+        [1,2,3,0,2]
+      ],
+      want: 3
+    }
+  ];
+  for (const tc of cases) {
+    expect(func.apply(this, tc.input), JSON.stringify(tc.input)).toBe(tc.want);
+  }
+});
