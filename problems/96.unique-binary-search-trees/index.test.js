41d3bf526a42ba5fa6f75c4246c2d739b99af3c4
tyx1703 <tyx1703@gmail.com>
2021-12-03 13:41:53 +0800

    96. 不同的二叉搜索树

---
+++ problems/96.unique-binary-search-trees/index.test.js
@@ -0,0 +1,21 @@
+import func from '.';
+
+test('96. 不同的二叉搜索树', () => {
+  const cases = [
+    {
+      input: [3],
+      want: 5,
+    },
+    {
+      input: [1],
+      want: 1,
+    },
+    {
+      input: [4],
+      want: 14,
+    },
+  ];
+  for (const tc of cases) {
+    expect(func.apply(this, tc.input), JSON.stringify(tc.input)).toBe(tc.want);
+  }
+});
