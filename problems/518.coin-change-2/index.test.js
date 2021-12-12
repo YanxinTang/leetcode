6d7e56f7ad26e7a7cdf2882e4637de5394bd1e0d
tyx1703 <tyx1703@gmail.com>
2021-12-12 17:46:45 +0800

    518. 零钱兑换 II

---
+++ problems/518.coin-change-2/index.test.js
@@ -0,0 +1,21 @@
+import func from '.';
+
+test('518. 零钱兑换 II', () => {
+  const cases = [
+    {
+      input: [5, [1, 2, 5]],
+      want: 4,
+    },
+    {
+      input: [3, [2]],
+      want: 0,
+    },
+    {
+      input: [10, [10]],
+      want: 1,
+    },
+  ];
+  for (const tc of cases) {
+    expect(func.apply(this, tc.input), JSON.stringify(tc.input)).toBe(tc.want);
+  }
+});
